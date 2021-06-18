1a98c84467bb16e2ef8b2ce81c351717c933c8e0
yaji <yajilin@fontrip.com>
2021-06-18 17:03:16 +0800

    refresh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>I'm testing</p>
+				<p>it response at the same time when I save</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
