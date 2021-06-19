d19e51076d042232f3fc0e6ed81a44d9119dfb34
yaji <yajilin@fontrip.com>
2021-06-19 21:03:14 +0800

    change text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Jessie Hsu</p>
+				<p>Hello Jessie Hsu !!!</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
